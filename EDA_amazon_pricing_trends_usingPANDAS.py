717b5bf4c1ce3b5b0ae1968bbfcf65118ccb8f11
Tadiparthi Gnaneswar <gnanitadiparthi@gmail.com>
2026-06-18 11:08:50 +0530

    Refactor EDA script for Amazon pricing trends

--- EDA_amazon_pricing_trends_usingPANDAS.py
+++ EDA_amazon_pricing_trends_usingPANDAS.py
@@ -17,7 +17,7 @@ for col in ['discounted_price', 'actual_price', 'discount_percentage', 'rating',
 df['price_diff'] = df['actual_price'] - df['discounted_price']
 df['discount_ratio'] = df['price_diff'] / df['actual_price']
 
-#Correlation analysis
+# Correlation analysis
 corr = df[['actual_price','discounted_price','discount_percentage','rating','rating_count','price_diff','discount_ratio']].corr()
 
 plt.figure(figsize=(8,6))
